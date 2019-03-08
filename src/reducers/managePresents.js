6dcc0ad67333f114c3e08ccf143a1e6bb99694aa
Vanessa Prema <vanessa.prema@flatironschool.com>
2019-03-08 07:43:10 -0500

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,10 @@
-export function managePresents(state, action){
-}
+import { statSync } from "fs";
+
+export function managePresents(state, action) {
+   switch(action.type) {
+      case "INCREASE":
+         return {numberOfPresents: state.numberOfPresents + 1}
+      default:
+         return state;
+   }
+};
